2354451e5c196ef3becb450ebdc0e8f4132083d1
Alec Smecher <alec@smecher.bc.ca>
2018-11-19 12:49:07 -0800

    Copyright dates

--- SubscriptionSSOPlugin.inc.php
+++ SubscriptionSSOPlugin.inc.php
@@ -3,8 +3,8 @@
 /**
  * @file plugins/generic/subscriptionSSO/SubscriptionSSOPlugin.inc.php
  *
- * Copyright (c) 2014-2017 Simon Fraser University
- * Copyright (c) 2014-2017 John Willinsky
+ * Copyright (c) 2014-2018 Simon Fraser University
+ * Copyright (c) 2014-2018 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file COPYING.
  *
  * @package plugins_generic_subscriptionSSO
