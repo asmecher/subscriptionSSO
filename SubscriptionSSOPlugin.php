a9f4328f29cff808583ad1629997701b37f38ac2
Alec Smecher <alec@smecher.bc.ca>
2023-04-06 18:18:21 -0700

    Update to version 1.0.4.0

--- SubscriptionSSOPlugin.php
+++ SubscriptionSSOPlugin.php
@@ -3,8 +3,8 @@
 /**
  * @file SubscriptionSSOPlugin.inc.php
  *
- * Copyright (c) 2014-2021 Simon Fraser University
- * Copyright (c) 2014-2021 John Willinsky
+ * Copyright (c) 2014-2023 Simon Fraser University
+ * Copyright (c) 2014-2023 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
  *
  * Plugin to defer subscription checks to an external system.
