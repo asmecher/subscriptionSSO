5bb6ef26b9e02a324ee81d16e11108efb0278c4d
Alec Smecher <alec@smecher.bc.ca>
2025-11-13 07:29:29 -0800

    Prevent SubscriptionSSO plugin from interfering with open access content in subscription issues

--- SubscriptionSSOPlugin.inc.php
+++ SubscriptionSSOPlugin.inc.php
@@ -86,11 +86,15 @@ class SubscriptionSSOPlugin extends GenericPlugin {
 		// Permit an abstract view.
 		if ($request->getRequestedPage() == 'article' && $request->getRequestedOp() == 'view' && count($request->getRequestedArgs())==1) return false;
 
-		$journal = $args[1];
+		$articleId = $args[3];
+		$submissionDao = DAORegistry::getDAO('SubmissionDAO'); /* @var $submissionDao SubmissionDAO */
+		$submission = $submissionDao->getById($articleId);
+		if ($submission && $submission->getCurrentPublication()->getData('accessStatus') == ARTICLE_ACCESS_OPEN) return false;
 
 		$result =& $args[4]; // Reference required
 		if ($result) return false; // If a subscription has already been established, respect that
 
+		$journal = $args[1];
 		$result = isset($_SESSION['subscriptionSSOTimestamp']) && $_SESSION['subscriptionSSOTimestamp'] + ($this->getSetting($journal->getId(), 'hoursValid') * 3600) + 1 >= time();
 		if (!$result) {
 			// If we're not subscribed, redirect.
