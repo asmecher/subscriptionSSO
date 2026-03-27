d73e62e8891cd605c668ae5042c3c5340c944b4f
Alec Smecher <alec@smecher.bc.ca>
2026-03-27 15:10:54 -0700

    Adapt for OJS 3.5.0

--- SubscriptionSSOPlugin.php
+++ SubscriptionSSOPlugin.php
@@ -3,8 +3,8 @@
 /**
  * @file SubscriptionSSOPlugin.inc.php
  *
- * Copyright (c) 2014-2023 Simon Fraser University
- * Copyright (c) 2014-2023 John Willinsky
+ * Copyright (c) 2014-2026 Simon Fraser University
+ * Copyright (c) 2014-2026 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
  *
  * Plugin to defer subscription checks to an external system.
@@ -17,157 +17,155 @@ use PKP\plugins\GenericPlugin;
 use PKP\linkAction\request\AjaxModal;
 use PKP\config\Config;
 use PKP\plugins\Hook;
+use APP\facades\Repo;
+use PKP\db\DAORegistry;
 use APP\template\TemplateManager;
 use PKP\core\JSONMessage;
 use APP\core\Application;
 
 class SubscriptionSSOPlugin extends GenericPlugin {
-	/**
-	 * @copydoc GenericPlugin::register
-	 */
-	function register($category, $path, $mainContextId = null) {
-		$success = parent::register($category, $path, $mainContextId);
-		if (!Config::getVar('general', 'installed') || defined('RUNNING_UPGRADE')) return true;
-		if ($success && $this->getEnabled()) {
-			$this->addLocaleData();
-			Hook::add('LoadHandler', [&$this, 'loadHandlerCallback']);
-			Hook::add('IssueAction::subscribedUser', [&$this, 'subscribedUserCallback']);
-			return true;
-		}
-		return $success;
-	}
+    /**
+     * @copydoc GenericPlugin::register
+     */
+    function register($category, $path, $mainContextId = null) {
+        $success = parent::register($category, $path, $mainContextId);
+        if (!Config::getVar('general', 'installed') || defined('RUNNING_UPGRADE')) return true;
+        if ($success && $this->getEnabled()) {
+            $this->addLocaleData();
+            Hook::add('LoadHandler', $this->loadHandlerCallback(...));
+            Hook::add('IssueAction::subscribedUser', $this->subscribedUserCallback(...));
+            return true;
+        }
+        return $success;
+    }
 
-	/**
-	 * Callback when a handler is loaded. Used to check for the presence
-	 * of an incoming authentication, which needs to be verified.
-	 * @param string $hookName Hook name
-	 * @param array $args Hook arguments
-	 * @return boolean Hook return status
-	 */
-	function loadHandlerCallback($hookName, $args) {
-		$request = Application::get()->getRequest();
-		$journal = $request->getJournal();
-		if (!$journal) return false;
+    /**
+     * Callback when a handler is loaded. Used to check for the presence
+     * of an incoming authentication, which needs to be verified.
+     */
+    function loadHandlerCallback(string $hookName, array $args) : bool
+    {
+        $request = Application::get()->getRequest();
+        $journal = $request->getJournal();
+        if (!$journal) return Hook::CONTINUE;
 
-		$incomingParameterName = $this->getSetting($journal->getId(), 'incomingParameterName');
-		// Using $_GET rather than Request because this may be case
-		// sensitive (e.g. differentiating myid from myId)
-		if ($incomingParameterName != '' && isset($_GET[$incomingParameterName])) {
-			$incomingKey = $_GET[$incomingParameterName];
+        $incomingParameterName = $this->getSetting($journal->getId(), 'incomingParameterName');
+        // Using $_GET rather than Request because this may be case
+        // sensitive (e.g. differentiating myid from myId)
+        if ($incomingParameterName != '' && isset($_GET[$incomingParameterName])) {
+            $incomingKey = $_GET[$incomingParameterName];
 
-			// This is an incoming authorization. Contact the remote service.
-			$verificationUrl = $this->getSetting($journal->getId(), 'verificationUrl');
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, $verificationUrl . urlencode($incomingKey));
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1) ;
-			curl_setopt($ch, CURLOPT_HEADER, 0);
-			$result = curl_exec($ch);
-			curl_close($ch);
+            // This is an incoming authorization. Contact the remote service.
+            $verificationUrl = $this->getSetting($journal->getId(), 'verificationUrl');
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $verificationUrl . urlencode($incomingKey));
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1) ;
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            $result = curl_exec($ch);
+            curl_close($ch);
 
-			// Verify the result.
-			$resultRegexp = $this->getSetting($journal->getId(), 'resultRegexp');
-			if (preg_match($resultRegexp, $result)) {
-				// Successfully validated.
-				$_SESSION['subscriptionSSOTimestamp'] = time();
-			} else {
-				// Failed to validate.
-				unset($_SESSION['subscriptionSSOTimestamp']);
-				$request->redirectUrl($this->getSetting($journal->getId(), 'redirectUrl'));
-			}
-		}
-		return false;
-	}
+            // Verify the result.
+            $resultRegexp = $this->getSetting($journal->getId(), 'resultRegexp');
+            if (preg_match($resultRegexp, $result)) {
+                // Successfully validated.
+                $request->getSession()->put('subscriptionSSOTimestamp', time());
+            } else {
+                // Failed to validate.
+                $request->getSession()->remove('subscriptionSSOTimestamp');
+                $request->redirectUrl($this->getSetting($journal->getId(), 'redirectUrl'));
+            }
+        }
+        return Hook::CONTINUE;
+    }
 
-	/**
-	 * Callback when a handler is loaded. Used to check for the presence
-	 * of an incoming authentication, which needs to be verified.
-	 * @param string $hookName Hook name
-	 * @param array $args Hook arguments
-	 * @return boolean Hook return status
-	 */
-	function subscribedUserCallback($hookName, $args) {
-		// Exclude the index and issue pages.
-		$request = Application::get()->getRequest();
-		if (in_array($request->getRequestedPage(), ['', 'index', 'search'])) return false;
-		// Capture issue galley requests, but not e.g. issue archive
-		if ($request->getRequestedPage() == 'issue' && count($request->getRequestedArgs()) != 2) return false;
+    /**
+     * Callback when a handler is loaded. Used to check for the presence
+     * of an incoming authentication, which needs to be verified.
+     */
+    function subscribedUserCallback(string $hookName, array $args) : bool
+    {
+        // Exclude the index and issue pages.
+        $request = Application::get()->getRequest();
+        if (in_array($request->getRequestedPage(), ['', 'index', 'search'])) return Hook::CONTINUE;
+        // Capture issue galley requests, but not e.g. issue archive
+        if ($request->getRequestedPage() == 'issue' && count($request->getRequestedArgs()) != 2) return Hook::CONTINUE;
 
-		// Permit an abstract view.
-		if ($request->getRequestedPage() == 'article' && $request->getRequestedOp() == 'view' && count($request->getRequestedArgs())==1) return false;
+        // Permit an abstract view.
+        if ($request->getRequestedPage() == 'article' && $request->getRequestedOp() == 'view' && count($request->getRequestedArgs())==1) return Hook::CONTINUE;
 
-		$articleId = $args[3];
-		$submissionDao = DAORegistry::getDAO('SubmissionDAO'); /* @var $submissionDao SubmissionDAO */
-		$submission = $submissionDao->getById($articleId);
-		if ($submission && $submission->getCurrentPublication()->getData('accessStatus') == ARTICLE_ACCESS_OPEN) return false;
+        $articleId = $args[3];
+        $submission = Repo::submission()->get($articleId);
+        if ($submission && $submission->getCurrentPublication()->getData('accessStatus') == ARTICLE_ACCESS_OPEN) return Hook::CONTINUE;
 
-		$result =& $args[4]; // Reference required
-		if ($result) return false; // If a subscription has already been established, respect that
+        $result =& $args[4]; // Reference required
+        if ($result) return Hook::CONTINUE; // If a subscription has already been established, respect that
 
-		$journal = $args[1];
-		$result = isset($_SESSION['subscriptionSSOTimestamp']) && $_SESSION['subscriptionSSOTimestamp'] + ($this->getSetting($journal->getId(), 'hoursValid') * 3600) + 1 >= time();
-		if (!$result) {
-			// If we're not subscribed, redirect.
-			$request->redirectUrl($this->getSetting($journal->getId(), 'redirectUrl') . '?redirectUrl=' . urlencode($request->getRequestUrl()));
-		}
-	}
+        $journal = $args[1];
+        $result = $request->getSession()->get('subscriptionSSOTimestamp', 0) + ($this->getSetting($journal->getId(), 'hoursValid') * 3600) + 1 >= time();
+        if (!$result) {
+            // If we're not subscribed, redirect.
+            $request->redirectUrl($this->getSetting($journal->getId(), 'redirectUrl') . '?redirectUrl=' . urlencode($request->getRequestUrl()));
+        }
+        return Hook::CONTINUE;
+    }
 
-	/**
-	 * @copydoc Plugin::getActions()
-	 */
-	function getActions($request, $actionArgs) {
-		$router = $request->getRouter();
-		return array_merge(
-			$this->getEnabled()?[
-				new LinkAction(
-					'settings',
-					new AjaxModal(
-						$router->url($request, null, null, 'manage', null, array_merge($actionArgs, array('verb' => 'settings'))),
-						$this->getDisplayName()
-					),
-					__('manager.plugins.settings'),
-					null
-				),
-			]:[],
-			parent::getActions($request, $actionArgs)
-		);
-	}
+    /**
+     * @copydoc Plugin::getActions()
+     */
+    function getActions($request, $actionArgs) {
+        $router = $request->getRouter();
+        return array_merge(
+            $this->getEnabled()?[
+                new LinkAction(
+                    'settings',
+                    new AjaxModal(
+                        $router->url($request, null, null, 'manage', null, array_merge($actionArgs, ['verb' => 'settings'])),
+                        $this->getDisplayName()
+                    ),
+                    __('manager.plugins.settings'),
+                    null
+                ),
+            ]:[],
+            parent::getActions($request, $actionArgs)
+        );
+    }
 
-	/**
-	 * @copydoc PKPPlugin::manage()
-	 */
-	function manage($args, $request) {
-		$context = $request->getContext();
-		$templateMgr = TemplateManager::getManager($request);
+    /**
+     * @copydoc PKPPlugin::manage()
+     */
+    function manage($args, $request) {
+        $context = $request->getContext();
+        $templateMgr = TemplateManager::getManager($request);
 
-		switch ($request->getUserVar('verb')) {
-			case 'settings':
-				$form = new SubscriptionSSOSettingsForm($this, $context->getId());
-				if ($request->getUserVar('save')) {
-					$form->readInputData();
-					if ($form->validate()) {
-						$form->execute();
-						return new JSONMessage();
-					}
-				} else {
-					$form->initData();
-				}
-				return new JSONMessage(true, $form->fetch($request));
-		}
-		return parent::manage($args, $request);
-	}
+        switch ($request->getUserVar('verb')) {
+            case 'settings':
+                $form = new SubscriptionSSOSettingsForm($this, $context->getId());
+                if ($request->getUserVar('save')) {
+                    $form->readInputData();
+                    if ($form->validate()) {
+                        $form->execute();
+                        return new JSONMessage();
+                    }
+                } else {
+                    $form->initData();
+                }
+                return new JSONMessage(true, $form->fetch($request));
+        }
+        return parent::manage($args, $request);
+    }
 
-	/**
-	 * @copydoc Plugin::getDisplayName
-	 */
-	function getDisplayName() {
-		return __('plugins.generic.subscriptionSSO.name');
-	}
+    /**
+     * @copydoc Plugin::getDisplayName
+     */
+    function getDisplayName() {
+        return __('plugins.generic.subscriptionSSO.name');
+    }
 
-	/**
-	 * @copydoc Plugin::getDescription
-	 */
-	function getDescription() {
-		return __('plugins.generic.subscriptionSSO.description');
-	}
+    /**
+     * @copydoc Plugin::getDescription
+     */
+    function getDescription() {
+        return __('plugins.generic.subscriptionSSO.description');
+    }
 }
 
--- SubscriptionSSOSettingsForm.php
+++ SubscriptionSSOSettingsForm.php
@@ -3,8 +3,8 @@
 /**
  * @file SubscriptionSSOSettingsForm.inc.php
  *
- * Copyright (c) 2014-2023 Simon Fraser University
- * Copyright (c) 2014-2023 John Willinsky
+ * Copyright (c) 2014-2026 Simon Fraser University
+ * Copyright (c) 2014-2026 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
  *
  * @class SubscriptionSSOSettingsForm
@@ -18,76 +18,77 @@ use APP\template\TemplateManager;
 
 class SubscriptionSSOSettingsForm extends Form {
 
-	/** @var int */
-	var $_journalId;
+    var int $_journalId;
 
-	/** @var GenericPlugin */
-	var $_plugin;
+    var SubscriptionSSOPlugin $_plugin;
 
-	/**
-	 * Constructor
-	 * @param object $plugin
-	 * @param int $journalId
-	 */
-	function __construct($plugin, $journalId) {
-		$this->_journalId = $journalId;
-		$this->_plugin = $plugin;
+    /**
+     * Constructor
+     */
+    function __construct(SubscriptionSSOPlugin $plugin, int $journalId)
+    {
+        $this->_journalId = $journalId;
+        $this->_plugin = $plugin;
 
-		parent::__construct($plugin->getTemplateResource('settingsForm.tpl'));
+        parent::__construct($plugin->getTemplateResource('settingsForm.tpl'));
 
-		$this->addCheck(new \PKP\form\validation\FormValidatorRegExp($this, 'incomingParameterName', 'required', 'plugins.generic.subscriptionSSO.settings.incomingParameterName.required', '/^[a-zA-Z0-9\/._-]+$/'));
-		$this->addCheck(new \PKP\form\validation\FormValidatorUrl($this, 'verificationUrl', 'required', 'plugins.generic.subscriptionSSO.settings.verificationUrl.required'));
-		$this->addCheck(new \PKP\form\validation\FormValidator($this, 'resultRegexp', 'required', 'plugins.generic.subscriptionSSO.settings.resultRegexp.required'));
-		$this->addCheck(new \PKP\form\validation\FormValidatorUrl($this, 'redirectUrl', 'required', 'plugins.generic.subscriptionSSO.settings.redirectUrl.required'));
-		$this->addCheck(new \PKP\form\validation\FormValidatorRegExp($this, 'hoursValid', 'required', 'plugins.generic.subscriptionSSO.settings.hoursValid.required', '/^[0-9]+$/'));
-	}
+        $this->addCheck(new \PKP\form\validation\FormValidatorRegExp($this, 'incomingParameterName', 'required', 'plugins.generic.subscriptionSSO.settings.incomingParameterName.required', '/^[a-zA-Z0-9\/._-]+$/'));
+        $this->addCheck(new \PKP\form\validation\FormValidatorUrl($this, 'verificationUrl', 'required', 'plugins.generic.subscriptionSSO.settings.verificationUrl.required'));
+        $this->addCheck(new \PKP\form\validation\FormValidator($this, 'resultRegexp', 'required', 'plugins.generic.subscriptionSSO.settings.resultRegexp.required'));
+        $this->addCheck(new \PKP\form\validation\FormValidatorUrl($this, 'redirectUrl', 'required', 'plugins.generic.subscriptionSSO.settings.redirectUrl.required'));
+        $this->addCheck(new \PKP\form\validation\FormValidatorRegExp($this, 'hoursValid', 'required', 'plugins.generic.subscriptionSSO.settings.hoursValid.required', '/^[0-9]+$/'));
+    }
 
-	/**
-	 * Initialize form data.
-	 */
-	function initData() {
-		$journalId = $this->_journalId;
-		$plugin = $this->_plugin;
+    /**
+     * Initialize form data.
+     */
+    function initData()
+    {
+        $journalId = $this->_journalId;
+        $plugin = $this->_plugin;
 
-		$this->_data = [
-			'incomingParameterName' => $plugin->getSetting($journalId, 'incomingParameterName'),
-			'verificationUrl' => $plugin->getSetting($journalId, 'verificationUrl'),
-			'resultRegexp' => $plugin->getSetting($journalId, 'resultRegexp'),
-			'redirectUrl' => $plugin->getSetting($journalId, 'redirectUrl'),
-			'hoursValid' => $plugin->getSetting($journalId, 'hoursValid')
-		];
-	}
+        $this->_data = [
+            'incomingParameterName' => $plugin->getSetting($journalId, 'incomingParameterName'),
+            'verificationUrl' => $plugin->getSetting($journalId, 'verificationUrl'),
+            'resultRegexp' => $plugin->getSetting($journalId, 'resultRegexp'),
+            'redirectUrl' => $plugin->getSetting($journalId, 'redirectUrl'),
+            'hoursValid' => $plugin->getSetting($journalId, 'hoursValid')
+        ];
+    }
 
-	/**
-	 * Assign form data to user-submitted data.
-	 */
-	function readInputData() {
-		$this->readUserVars(['incomingParameterName', 'verificationUrl', 'resultRegexp', 'redirectUrl', 'hoursValid']);
-	}
+    /**
+     * Assign form data to user-submitted data.
+     */
+    function readInputData()
+    {
+        $this->readUserVars(['incomingParameterName', 'verificationUrl', 'resultRegexp', 'redirectUrl', 'hoursValid']);
+    }
 
-	/**
-	 * @copydoc Form::fetch()
-	 */
-	function fetch($request, $template = null, $display = false) {
-		$templateMgr = TemplateManager::getManager($request);
-		$templateMgr->assign('pluginName', $this->_plugin->getName());
-		return parent::fetch($request, $template, $display);
-	}
+    /**
+     * @copydoc Form::fetch()
+     */
+    function fetch($request, $template = null, $display = false)
+    {
+        $templateMgr = TemplateManager::getManager($request);
+        $templateMgr->assign('pluginName', $this->_plugin->getName());
+        return parent::fetch($request, $template, $display);
+    }
 
-	/**
-	 * @copydoc Form::execute()
-	 */
-	function execute(...$functionArgs) {
-		$plugin = $this->_plugin;
-		$journalId = $this->_journalId;
+    /**
+     * @copydoc Form::execute()
+     */
+    function execute(...$functionArgs)
+    {
+        $plugin = $this->_plugin;
+        $journalId = $this->_journalId;
 
-		$plugin->updateSetting($journalId, 'incomingParameterName', $this->getData('incomingParameterName'), 'string');
-		$plugin->updateSetting($journalId, 'verificationUrl', $this->getData('verificationUrl'), 'string');
-		$plugin->updateSetting($journalId, 'resultRegexp', $this->getData('resultRegexp'), 'string');
-		$plugin->updateSetting($journalId, 'redirectUrl', $this->getData('redirectUrl'), 'string');
-		$plugin->updateSetting($journalId, 'hoursValid', $this->getData('hoursValid'), 'string');
+        $plugin->updateSetting($journalId, 'incomingParameterName', $this->getData('incomingParameterName'), 'string');
+        $plugin->updateSetting($journalId, 'verificationUrl', $this->getData('verificationUrl'), 'string');
+        $plugin->updateSetting($journalId, 'resultRegexp', $this->getData('resultRegexp'), 'string');
+        $plugin->updateSetting($journalId, 'redirectUrl', $this->getData('redirectUrl'), 'string');
+        $plugin->updateSetting($journalId, 'hoursValid', $this->getData('hoursValid'), 'string');
 
-		parent::execute(...$functionArgs);
-	}
+        parent::execute(...$functionArgs);
+    }
 }
 
